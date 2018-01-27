f12e1c09cb34403f6110c256a6783b246147f291
Robin Ting <civ2boss@gmail.com>
2018-01-27 08:54:09 -0500

    Add slideshow logic and styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,11 +4,86 @@ import Search from './search.js';
 import Slideshow from './slideshow.js';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selected: {},
+      selectedIndex: 0,
+      photos: [],
+      tag: 'puppy'
+    };
+
+    this.selectPhoto = this.selectPhoto.bind(this);
+    this.prevPhoto = this.prevPhoto.bind(this);
+    this.nextPhoto = this.nextPhoto.bind(this);
+  }
+
+  flickrPhotoUrl(photo, format) {
+    const farmID = photo.farm;
+    const serverID = photo.server;
+    const id = photo.id;
+    const secret = photo.secret;
+    return `https://farm${farmID}.staticflickr.com/${serverID}/${id}_${secret}_${format}.jpg`;
+  }
+
+  selectPhoto(photo, index) {
+    this.setState({
+      selected: photo,
+      selectedIndex: index
+    });
+  }
+
+  prevPhoto(e) {
+    e.preventDefault();
+    const newIndex =
+      this.state.selectedIndex === 0
+        ? this.state.photos.length - 1
+        : this.state.selectedIndex - 1;
+    this.setState({
+      selected: this.state.photos[newIndex],
+      selectedIndex: newIndex
+    });
+  }
+
+  nextPhoto(e) {
+    e.preventDefault();
+    const newIndex =
+      this.state.selectedIndex === this.state.photos.length - 1
+        ? 0
+        : this.state.selectedIndex + 1;
+    this.setState({
+      selected: this.state.photos[newIndex],
+      selectedIndex: newIndex
+    });
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://api.flickr.com/services/rest/?api_key=7b07ad6356a53f942bd7453bdc60f7e0&method=flickr.photos.search&tags=puppy&format=json&nojsoncallback=1`
+    )
+      .then(res => res.json())
+      .then(response => {
+        this.setState({
+          selected: response.photos.photo[0],
+          selectedIndex: 0,
+          photos: response.photos.photo
+        });
+      });
+  }
+
   render() {
     return (
       <div className="app">
         <Search />
-        <Slideshow />
+        <Slideshow
+          selected={this.state.selected}
+          photos={this.state.photos}
+          flickrPhotoUrl={this.flickrPhotoUrl}
+          selectPhoto={this.selectPhoto}
+          prevPhoto={this.prevPhoto}
+          nextPhoto={this.nextPhoto}
+        />
       </div>
     );
   }
--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -1,22 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Slideshow extends React.Component {
   render() {
     return (
       <div className="slideshow">
         <div className="viewer">
-          <div className="image" />
+          <div className="image">
+            <img
+              src={this.props.flickrPhotoUrl(this.props.selected, 'c')}
+              alt=""
+            />
+          </div>
           <div className="arrow arrow-left">
-            <a href="">&lt;</a>
+            <a href="" onClick={this.props.prevPhoto}>
+              &lt;
+            </a>
           </div>
           <div className="arrow arrow-right">
-            <a href="">&gt;</a>
+            <a href="" onClick={this.props.nextPhoto}>
+              &gt;
+            </a>
           </div>
         </div>
-        <div className="thumbnails" />
+        <div className="thumbnails">
+          <ul>
+            {this.props.photos.map((photo, i) => (
+              <li
+                key={i}
+                className={
+                  Object.is(this.props.selected, photo) ? 'active' : ''
+                }
+                onClick={this.props.selectPhoto.bind(this, photo, i)}
+              >
+                <img src={this.props.flickrPhotoUrl(photo, 't')} alt="" />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
 }
+
+Slideshow.propTypes = {
+  selected: PropTypes.object,
+  photos: PropTypes.array,
+  flickrPhotoUrl: PropTypes.func,
+  selectPhoto: PropTypes.func,
+  prevPhoto: PropTypes.func,
+  nextPhoto: PropTypes.func
+};
 
 export default Slideshow;
